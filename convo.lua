4ff07c781ba8626072b9285647b4c49101428c97
Benedict Ide <benedictide@pop-os.localdomain>
2023-07-23 14:15:01 -0400

    Convos write out strings and progress

--- convo.lua
+++ convo.lua
@@ -7,25 +7,47 @@ Convo = function(convoFile)
 	for i=1,#c.data.lines,1 do
 		local line = c.data.lines[i];
 		if line.id then 
-			conv.idIndices[line.id] = i;
+			c.idIndices[line.id] = i;
 		end
 	end
     c.box = TextBox();
     c.start = function()
-        c.box.rise();
+        c.box.rise(function() 
+            c.box.setLine(c.data.lines[c.line]);
+        end);
+    end
+    c.advance = function() 
+        local line = c.data.lines[c.line];
+        if line.jump then
+            c.line = c.idIndices[line.to]
+        else
+            c.line = c.line + 1;
+            if c.line > #(c.data.lines) then
+                c.conclude();
+                return;
+            end--else
+            DEBUG_TEXT = "attempting to set line: " .. c.data.lines[c.line].text;
+            c.box.setLine(c.data.lines[c.line]);
+            c.box.state = "WRITE";
+        end
+    end
+    c.conclude = function()
+        c.box.state = "TRANSITION";
+        c.box.fall(function()
+            game.battle.endUnitsTurn(game.battle.actionMenu.unit);
+        end);
     end
     c.render = function()
         c.box.render();
     end
     c.update = function()
         if c.box.state == "WRITE" then
+            c.box.update();
+        elseif c.box.state == "HOLD" then
             local mouseinput = pressedThisFrame.mouse1;
             local otherinput = pressedThisFrame.action;
             if mouseinput or otherinput then
-                c.box.state = "TRANSITION";
-                c.box.fall(function()
-                    game.battle.endUnitsTurn(game.battle.actionMenu.unit);
-                end);
+                c.advance();
             end
         end
     end
--- textbox.lua
+++ textbox.lua
@@ -1,4 +1,7 @@
 textboxImg = love.graphics.newImage("assets/img/textbg.png");
+TEXT_MIN_CHARS_TO_SPEED = 6;
+TEXT_SPEED = 0.5;
+TEXT_FASTSPEED = 2.5;
 TextBox = function()
     local tb = {};
     tb.state = "TRANSITION"; --WRITE, HOLD, SELECT
@@ -12,9 +15,9 @@ TextBox = function()
         tb.portraits[id] = tbp;
         tb.portraits.push(tbp);
     end
-    tb.registerPortrait("default","assets/img/herotall.png");
 
     -- v TODO: remove after testing:
+    tb.registerPortrait("default","assets/img/herotall.png");
     tb.portraits["default"].active = true;
     tb.portraits["default"].lit = true;
     tb.registerPortrait("archer","assets/img/archtall.png");
@@ -22,9 +25,16 @@ TextBox = function()
     tb.portraits["archer"].x = 300;
     -- ^ TODO: remove after testing:
 
-    tb.testString = "some <b>bold</b> <c=#00DD66><i>green italic</i></c> <b><i>bolditalic</i></b> <c=#00AAFF>text</c> that goes on for a little <b>while</b> and would do some wrapping. it's going to get cut off at 120 characters, i think, so add a bunch more characters to the string.";
-    tb.calligrapher.fstrings = TextFormatter.getFormattedStrings(tb.testString);
+    tb.charMax = 0;
 
+    tb.testString = "some <b>bold</b> <c=#00DD66><i>green italic</i></c> <b><i>bolditalic</i></b> <c=#00AAFF>text</c> that goes on for a little <b>while</b> and would do some wrapping. it's going to get cut off at 120 characters, i think, so add a bunch more characters to the string.";
+    --tb.calligrapher.fstrings = TextFormatter.getFormattedStrings(tb.testString);
+    tb.setLine = function(lineObj)
+        tb.calligrapher.fstrings = TextFormatter.getFormattedStrings(lineObj.text);
+        tb.charMax = TextFormatter.formattedStringLength(tb.calligrapher.fstrings);
+        tb.calligrapher.charsDrawn = 0;
+    end
+    --tb.setLine({text = tb.testString});
 
     tb.render = function()
         for i=1,#(tb.portraits),1 do
@@ -32,13 +42,15 @@ TextBox = function()
         end
         love.graphics.setColor(1,1,1);
         tb.box.draw(25,tb.box.y);
-        tb.calligrapher.draw();
+        if tb.calligrapher.fstrings then
+            tb.calligrapher.draw();
+        end
     end
-    tb.rise = function()
+    tb.rise = function(whendone)
         async.doOverTime(0.2,function(percent) 
             tb.box.y = gameheight - math.floor(percent * tb.box.h);
-            DEBUG_TEXT = "moving percent " .. percent;
         end,function() 
+            whendone();
             tb.box.y = gameheight - tb.box.h;
             --TODO: actually transition to some functioning write state
             tb.state = "WRITE"; 
@@ -52,8 +64,21 @@ TextBox = function()
             if whendone then whendone(); end
         end);
     end
+	tb.speed_60fps = 10;
     tb.update = function()
-        
+        if tb.state == "WRITE" then
+            if tb.calligrapher.charsDrawn < tb.charMax then
+                local dt = love.timer.getDelta();
+                local charspeed = ((input.action or input.cancel) and tb.calligrapher.charsDrawn > TEXT_MIN_CHARS_TO_SPEED) and TEXT_FASTSPEED or TEXT_SPEED;
+                charspeed = charspeed * dt * gameFPS;
+                tb.calligrapher.charsDrawn = tb.calligrapher.charsDrawn + charspeed;
+                if pressedThisFrame["menu"] then
+                    box.td.charsDrawn = box.charMax;
+                end
+            else
+                tb.state = "HOLD";
+            end
+        end
     end
     return tb;
 end
