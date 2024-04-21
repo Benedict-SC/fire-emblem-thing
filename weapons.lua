0f91342658e0e76130e873924a72c769cefd5fd8
Benedict Ide <benedictide@gmail.com>
2024-04-21 18:07:11 -0400

    Items load from custom data now

--- weapons.lua
+++ weapons.lua
@@ -1,5 +1,5 @@
-Weapon = function()
-    local wepon = {};
+Weapon = function(rawWeapon)
+    --[[local wepon = {};
     wepon.name = "Generic Weapon";
     wepon.iconfile = "assets/img/qmark-tiny.png";
     wepon.wtype = "NONE";
@@ -16,7 +16,14 @@ Weapon = function()
     end
     wepon.isWeapon = true;
     wepon.brave = false;
-    return wepon;
+    return wepon;--]]
+    rawWeapon.range = arrayify(rawWeapon.range)
+    if rawWeapon.wtype == "ITEM" then --TODO: there's non-usable held-items so check for that
+        rawWeapon.usable = function() return true; end
+    else
+        rawWeapon.usable = function() return false; end
+    end
+    return rawWeapon;
 end
 weaponCache = {};
 weaponCache.getInstance = function(wid)
@@ -25,80 +32,9 @@ weaponCache.getInstance = function(wid)
     return instance;
 end
 equipDot = love.graphics.newImage("assets/img/equip-dot.png");
---PRESET WEAPON DATA
---SWORDS
-weaponCache["IronSword"] = Weapon();
-weaponCache["IronSword"].name = "Iron Sword";
-weaponCache["IronSword"].wtype = "SWORD";
-weaponCache["IronSword"].iconfile = "assets/img/sword.png";
-weaponCache["IronSword"].might = 5;
-weaponCache["IronSword"].hit = 90;
-weaponCache["IronSword"].weight = 7;
-
-weaponCache["BraveSword"] = Weapon();
-weaponCache["BraveSword"].name = "Brave Sword";
-weaponCache["BraveSword"].wtype = "SWORD";
-weaponCache["BraveSword"].might = 9;
-weaponCache["BraveSword"].hit = 75;
-weaponCache["BraveSword"].brave = true;
-weaponCache["BraveSword"].rank = "B";
-weaponCache["BraveSword"].weight = 16;
-weaponCache["BraveSword"].maxUses = 30;
-
-weaponCache["TrashSword"] = Weapon();
-weaponCache["TrashSword"].name = "Trash Sword";
-weaponCache["TrashSword"].wtype = "SWORD";
-weaponCache["TrashSword"].iconfile = "assets/img/sword.png";
-weaponCache["TrashSword"].might = 3;
-weaponCache["TrashSword"].hit = 30;
-weaponCache["TrashSword"].weight = 5;
-weaponCache["TrashSword"].maxUses = 2;
-
---LANCES
-weaponCache["IronLance"] = Weapon();
-weaponCache["IronLance"].name = "Iron Lance";
-weaponCache["IronLance"].wtype = "LANCE";
-weaponCache["IronLance"].iconfile = "assets/img/lance.png";
-weaponCache["IronLance"].might = 7;
-
-weaponCache["SteelLance"] = Weapon();
-weaponCache["SteelLance"].name = "Steel Lance";
-weaponCache["SteelLance"].wtype = "LANCE";
-weaponCache["SteelLance"].iconfile = "assets/img/lance.png";
-weaponCache["SteelLance"].might = 10;
-weaponCache["SteelLance"].rank = "D";
-weaponCache["SteelLance"].hit = 70;
-weaponCache["SteelLance"].weight = 13;
-weaponCache["SteelLance"].maxUses = 30;
-
-weaponCache["Javelin"] = Weapon();
-weaponCache["Javelin"].name = "Javelin";
-weaponCache["Javelin"].wtype = "LANCE";
-weaponCache["Javelin"].iconfile = "assets/img/lance.png";
-weaponCache["Javelin"].might = 6;
-weaponCache["Javelin"].rank = "D";
-weaponCache["Javelin"].range = Array(1,2);
-weaponCache["Javelin"].hit = 60;
-weaponCache["Javelin"].weight = 11;
-weaponCache["Javelin"].maxUses = 25;
-
---BOWS
-weaponCache["IronBow"] = Weapon();
-weaponCache["IronBow"].name = "Iron Bow";
-weaponCache["IronBow"].wtype = "BOW";
-weaponCache["IronBow"].iconfile = "assets/img/bow.png";
-weaponCache["IronBow"].might = 6;
-weaponCache["IronBow"].hit = 85;
-weaponCache["IronBow"].range = Array(2);
-weaponCache["IronBow"].weight = 5;
-
---DARK MAGIC
-weaponCache["Flux"] = Weapon();
-weaponCache["Flux"].name = "Flux";
-weaponCache["Flux"].wtype = "DARK";
-weaponCache["Flux"].iconfile = "assets/img/book.png";
-weaponCache["Flux"].might = 7;
-weaponCache["Flux"].hit = 70;
-weaponCache["Flux"].crit = 5;
-weaponCache["Flux"].range = Array(1,2);
-weaponCache["Flux"].weight = 3;
+local weaponsJson = love.filesystem.read("custom/items.json");
+local weaponData = json.decode(weaponsJson);
+for i=1,#weaponData,1 do
+    local raw = weaponData[i];
+    weaponCache[raw.id] = Weapon(raw);
+end
